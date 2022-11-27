9242740aa06ed084c03698a0b245ca4210bd8f43
willianmenezess <willmenezes@outlook.com.br>
2022-11-27 16:34:46 -0300

    1004 - prod simples

--- iniciante/1004-produto-simples.js
+++ iniciante/1004-produto-simples.js
@@ -1,9 +1,10 @@
 var input = require('fs').readFileSync('/dev/stdin', 'utf8');
 var lines = input.split('\n');
 
+
 let a = parseInt(lines.shift());
 let b = parseInt(lines.shift());
 
-let soma = a + b;
+let prod = a * b;
 
-console.log("SOMA")
+console.log("PROD = " + prod);
